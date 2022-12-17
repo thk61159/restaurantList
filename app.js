140af994313587b494bc96516aaeb1689826b7d7
thk61159 <tina0757023.bt07@nycu.edu.tw>
2022-12-18 01:09:31 +0800

    feat: add list seeder and change import tools

--- app.js
+++ app.js
@@ -1,37 +1,55 @@
-//import express
-const express = require('express')
-const app = express()
-// test port:3000
-const port = 3000
-//import restaurantList
-const restaurantList = require('./restaurant.json')
-//import handlebars see handlebars instuction
-const { engine } = require("express-handlebars");
-// express template exphbs.engine
-app.engine("handlebars", engine());
-app.set("view engine", "handlebars");
-app.set("views", "./views");
-//import static css here is bootstrap
-app.use(express.static("public"));
-
-
+//express
+const express = require('express');
+const app = express();
+const port = 3000;
+//template engine
+const { engine } = require('express-handlebars');
+app.engine('hbs', engine({ defaultLayout: 'main', extname: '.hbs' }));
+app.set('view engine', 'hbs');
+app.set('views', './views');
+//static file
+app.use(express.static('public'));
+//環境變數
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+//database
+const mongoose = require('mongoose'); 
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}); 
+//資料庫連線狀態
+const db = mongoose.connection;
+db.on('error', () => {
+  console.log('mongodb error!');
+});
+db.once('open', () => {
+  console.log('mongodb connected!');
+});
+const List = require('./models/list')
+comsole.log(List)
+//////////////////////////////
 app.get('/', (req, res) => {
-  console.log(restaurantList);
-  res.render("index", { restaurantList: restaurantList.results });
-})
-app.get("/search", (req, res) => {
+  List.find()
+    .lean()
+    .then((list) => res.render('index', { list }))
+    .catch((error) => console.error(error));
+});
+app.get('/search', (req, res) => {
   const keyword = req.query.keyword;
   console.log(keyword);
+ 
   const searchedRestaurant = restaurantList.results.filter((e) =>
     e.name.toLowerCase().includes(keyword.toLowerCase())
   );
-  res.render("index", { restaurantList: searchedRestaurant, keyword: keyword });
+  res.render('index', { restaurantList: searchedRestaurant, keyword: keyword });
 });
-app.get("/restaurants/:id", (req, res) => {
+app.get('/restaurants/:id', (req, res) => {
   const showRestaurant = restaurantList.results.find(
     (e) => e.id.toString() === req.params.id
   );
-  res.render("show", { showRestaurant: showRestaurant });
+  res.render('show', { showRestaurant: showRestaurant });
 });
 
 // start and listen on the Express server
---
+++ models/seeds/listSeeder.js
@@ -0,0 +1,36 @@
+const restaurantJson = require('../../restaurant.json');
+const restaurantList = restaurantJson.results
+const mongoose = require('mongoose');
+const List = require('../list'); // 載入 todo model
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+restaurantList.forEach((element) => console.log(element.name));
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on('error', () => {
+  console.log('mongodb error!');
+});
+db.once('open', () => {
+  console.log('mongodb connected!');
+  // restaurantList.forEach(element => {
+  //   return List.create(
+  //     {
+  //       name: element.name,
+  //       name_en: element.name_en,
+  //       category: element.category,
+  //       image: element.image,
+  //       location: element.location,
+  //       phone: element.phone,
+  //       google_map: element.google_map,
+  //       rating: element.rating,
+  //       description: element.description
+  //     }
+  //   );
+  // });
+  console.log('done');
+});
+
