e42041c7b0c15c69eac0b817ac3b84bedc233f6a
thk61159 <tina0757023.bt07@nycu.edu.tw>
2023-02-06 02:08:24 +0800

    fix seeder file

--- models/seeds/listSeeder.js
+++ models/seeds/listSeeder.js
@@ -20,6 +20,7 @@ const SEED_USER = [
 		restaurantList: restaurantList.slice(3, 6),
 	},
 ]
+//promise
 db.once('open', () => {
 	Promise.all(
 		SEED_USER.map(user => {
@@ -34,9 +35,12 @@ db.once('open', () => {
 					})
 				)
 				.then(user => {
+					console.log(user)
 					const userId = user._id
+					user = SEED_USER.filter(e => e.name === user.name)
+					console.log(user)
 					return Promise.all(
-						restaurantList.map(element => {
+						user[0].restaurantList.map(element => {
 							return List.create({
 								name: element.name,
 								name_en: element.name_en,
@@ -58,3 +62,46 @@ db.once('open', () => {
 		})
 	)
 })
+//async fn
+// db.once('open', async () => {
+// 	try {
+// 		await SEED_USER.map(user => {
+// 			bcrypt
+// 				.genSalt(10)
+// 				.then(salt => bcrypt.hash(user.password, salt))
+// 				.then(hash =>
+// 					User.create({
+// 						name: user.name,
+// 						email: user.email,
+// 						password: hash,
+// 					})
+// 				)
+// 				.then(async user => {
+// 					const userId = user._id
+// 					try {
+// 						await user.restaurantList.map(element => {
+// 							return List.create({
+// 								name: element.name,
+// 								name_en: element.name_en,
+// 								category: element.category,
+// 								image: element.image,
+// 								location: element.location,
+// 								phone: element.phone,
+// 								google_map: element.google_map,
+// 								rating: element.rating,
+// 								description: element.description,
+// 								userId,
+// 							})
+// 						})
+// 					} catch (err) {
+// 						console.log(err)
+// 					}
+// 				})
+// 		})
+// 		console.log('done.')
+// 		process.exit()
+// 	} catch (err) {
+// 		console.log(err)
+// 	}
+	
+// })
