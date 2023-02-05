8c71c365c8ed000bc66d4a3f347e7360f08fd570
thk61159 <tina0757023.bt07@nycu.edu.tw>
2023-02-06 01:31:30 +0800

    feat: update seeder file

--- models/seeds/listSeeder.js
+++ models/seeds/listSeeder.js
@@ -1,51 +1,60 @@
 const restaurantJson = require('../../restaurant.json')
-const restaurantList = restaurantJson.results.slice(
-	0,
-	Math.floor(Math.random() * 8 + 1)
-)
-console.log(restaurantList)
+const restaurantList = restaurantJson.results
 const List = require('../list')
 const User = require('../user')
 // const { List, User } = require('../../models')
 const db = require('../../config/mongoose')
 const bcrypt = require('bcryptjs')
 
-const SEED_USER = {
-	name: 'root',
-	email: 'root@example.com',
-	password: '12345678',
-}
+const SEED_USER = [
+	{
+		name: 'user1',
+		email: 'user1@example.com',
+		password: '12345678',
+		restaurantList: restaurantList.slice(0, 3),
+	},
+	{
+		name: 'user2',
+		email: 'user2@example.com',
+		password: '12345678',
+		restaurantList: restaurantList.slice(3, 6),
+	},
+]
 db.once('open', () => {
-	bcrypt
-		.genSalt(10)
-		.then(salt => bcrypt.hash(SEED_USER.password, salt))
-		.then(hash =>
-			User.create({
-				name: SEED_USER.name,
-				email: SEED_USER.email,
-				password: hash,
-			})
-		)
-		.then(user => {
-			const userId = user._id
-			return Promise.all(
-				restaurantList.map(element => {
-					return List.create({
-						name: element.name,
-						name_en: element.name_en,
-						category: element.category,
-						image: element.image,
-						location: element.location,
-						phone: element.phone,
-						google_map: element.google_map,
-						rating: element.rating,
-						description: element.description,
-						userId,
+	Promise.all(
+		SEED_USER.map(user => {
+			bcrypt
+				.genSalt(10)
+				.then(salt => bcrypt.hash(user.password, salt))
+				.then(hash =>
+					User.create({
+						name: user.name,
+						email: user.email,
+						password: hash,
+					})
+				)
+				.then(user => {
+					const userId = user._id
+					return Promise.all(
+						restaurantList.map(element => {
+							return List.create({
+								name: element.name,
+								name_en: element.name_en,
+								category: element.category,
+								image: element.image,
+								location: element.location,
+								phone: element.phone,
+								google_map: element.google_map,
+								rating: element.rating,
+								description: element.description,
+								userId,
+							})
+						})
+					).then(() => {
+						console.log('done.')
+						process.exit()
 					})
 				})
-			).then(() => {
-				console.log('done.')
-				process.exit()
-			})
 		})
+	)
 })
